a40066122a876b34d0df6ffb099849af999683e7
aeroncalara <aeroncalara@gmail.com>
2019-04-16 15:50:57 +0800

    edit update

--- new-app/src/EmployeeComponents/DeleteEmployee.js
+++ new-app/src/EmployeeComponents/DeleteEmployee.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
 import { Button, Header, Icon, Modal } from 'semantic-ui-react'
 
-export default class ModalExampleControlled extends Component {
+
+
+
+
+export default class extends Component {
+
   state = { modalOpen: false }
 
   handleOpen = () => this.setState({ modalOpen: true })
 
   handleClose = () => this.setState({ modalOpen: false })
+
 
   render() {
     return (
@@ -14,8 +20,7 @@ export default class ModalExampleControlled extends Component {
         trigger={<Button className="ui button negative " onClick={this.handleOpen}>Delete</Button>}
         open={this.state.modalOpen}
         onClose={this.handleClose}
-        basic
-        size='small'
+        basic size='small'
       >
         <Header icon='frown outline' content='Delete Employee' />
         <Modal.Content>
@@ -23,15 +28,13 @@ export default class ModalExampleControlled extends Component {
         </Modal.Content>
         
         <Modal.Actions>
-
             <Button color='red' onClick={this.handleClose} inverted>
                 <Icon name='x' /> No
             </Button>
 
-          <Button color='green' onClick={this.handleClose} inverted>
-            <Icon name='checkmark' /> Yes
-          </Button>
-
+            <Button color='green' onClick={this.handleClose} inverted>
+              <Icon name='checkmark' /> Yes
+            </Button>
         </Modal.Actions>
       </Modal>
     )
--- new-app/src/EmployeeComponents/ViewEmployee.js
+++ new-app/src/EmployeeComponents/ViewEmployee.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react'
 import { Button, Image, Modal } from 'semantic-ui-react'
 import ViewEmployeeForm from '../EmployeeComponents/ViewEmployeeForm';
 
-export default class  extends Component {
+export default class extends Component {
+
+  state = { modalOpen: false }
+
+  handleOpen = () => this.setState({ modalOpen: true })
+
+  handleClose = () => this.setState({ modalOpen: false })
+
+
   render() {
     return (
           
-        <Modal trigger={<Button className="ui button positive ">View</Button>}>
+      <Modal
+      trigger={<Button className="ui button positive " onClick={this.handleOpen}>View</Button>}
+      open={this.state.modalOpen}
+      onClose={this.handleClose}
+     
+    >
         
         <Modal.Header>View Employee</Modal.Header>
           <Modal.Content>
