41ab03b3a5c437e5e02eea7d67dffc359f45968b
Lyka <lyka.rpinnotech@gmail.com>
2019-07-17 18:07:24 -0700

    dashboard navbar and sidebar \Lykaninja

--- new-app/src/Dashboard/DashboardPage.js
+++ new-app/src/Dashboard/DashboardPage.js
@@ -1,5 +1,12 @@
 import React, {Component} from 'react';
-import './Dashboard.css'
+import './Dashboard.css';
+import logo from '../Logo_rp.png';
+import iconfemale from '../iconfemale.png';
+import iconmail from '../iconmail.png';
+import iconnotif from '../iconnotif.png';
+import icondash from '../dash.png';
+import {Icon } from 'semantic-ui-react'
+
 
 
 
@@ -16,9 +23,27 @@ class Dashboard extends Component {
     render(){
         return(
             <div className = "dashPage">
-                <div className = "navDash">
+                <nav className = "navDash">
+                    <ul>
+                    <img src = {logo} className = "logo"></img>
+                        <li><a href = ""><img alt = "icon"src= {iconfemale} className = "icon"></img></a></li>
+                        <li><a href = ""><img alt = "icon" src={iconmail} className = "icon"></img></a></li>
+                        <li><a href = ""><img alt = "icon" src={iconnotif} className = "icon"></img></a></li>  
+                        </ul>
+                </nav> 
                 
+                <div className = "sideDash">
+                    <p className = "nameDash">Welcome,<a  href = ""> Admin!</a></p>
+                    <Icon name = "dashboard"></Icon>
+                    <a className = "tabs" href = "">Dashboard</a>
+                    <Icon name = "dashboard"></Icon>
+                    <a className = "tabs" href = "">Employee</a>
+                    <Icon name = "dashboard"></Icon>
+                    <a className = "tabs" href ="">Attendance</a>
+                    <Icon name = "dashboard"></Icon>
+                    <a className = "tabs" href = "">Payroll</a>
                 </div>
+                
             </div>
         )
     }
