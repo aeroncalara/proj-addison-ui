b7b77a4aee3140f2942ebaf9ab51c0d1001b8eab
aeroncalara <aeroncalara@gmail.com>
2020-05-20 09:04:02 +0800

    new commit

---
+++ node_modules/.history/new-app/src/EmployeeComponents/ViewEmployee_20200519105255.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+import { Popup, Button } from 'semantic-ui-react'
+
+export default class extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+        item: props.item
+        }
+    }
+
+    render() {
+
+        const {item} = this.state;
+
+        return (
+            
+            <div>
+                <Link to={"/main/employee/id/" +item._id}>
+                    <Popup
+                        trigger={<Button primary> View
+                        </Button>}
+                        content='View Employee Details'
+                        position='top center'
+                    />
+                </Link>
+            </div>    
+        )   
+    }
+}
+
+
+
